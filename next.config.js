639cbb04bcbb376e26764994833603b944dc5e64
Udhibhan <udaiyanbhan@gmail.com>
2026-04-11 17:43:54 +0800

    mqtt debug

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        net: false,
+        tls: false,
+        fs: false,
+        path: false,
+        os: false,
+        crypto: false,
+        stream: false,
+        buffer: false,
+      }
+    }
+    return config
+  },
 }
 module.exports = nextConfig
